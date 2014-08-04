7e3cfca35a25255614d0880de3e06a5b8dff5f94
Solixious <arcanist@programmer.net>
2014-08-04 10:47:54 +0530

    Update SpaceWarrior.java

--- SpaceWarrior.java
+++ SpaceWarrior.java
@@ -1,15 +1,33 @@
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 public class SpaceWarrior {
 	public static JFrame window;
 	
+	public static int WIDTH;
+	public static int HEIGHT;
+	
 	static {
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Dimension screen = tk.getScreenSize();
+		WIDTH = screen.width;
+		HEIGHT = screen.height;
 		window = new JFrame();
-		window.setSize(512, 512);
+		window.setSize(WIDTH, HEIGHT);
 		window.setLayout(null);
 		window.setFocusable(false);
 		window.setUndecorated(true);
+		window.setAlwaysOnTop(true);
+		
+		BufferedImage cursorImage = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+		Cursor blankCursor = tk.createCustomCursor(cursorImage, new Point(0,0), "Blank Cursor");
+		window.getContentPane().setCursor(blankCursor);
 	}
 	
 	public static void exit() {
