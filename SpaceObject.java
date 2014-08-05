9e0a5074294e32eaddb328fc5509b367c7cb6c30
Solixious <arcanist@programmer.net>
2014-08-05 10:26:38 +0530

    Update SpaceObject.java

--- SpaceObject.java
+++ SpaceObject.java
@@ -6,6 +6,12 @@ public interface SpaceObject {
 	public int getY();
 	public int getWidth();
 	public int getHeight();
+	public int getStrength();
+	
+	public boolean isOnScreen();
+	
 	public void move();
+	public void damaged(int v);
+	
 	public BufferedImage getImage();
 }
